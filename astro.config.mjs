2b6b8fa6447c33e881dc46da910d3a00c8b6746d
Akshat Navlani <akshatnavlani1@gmail.com>
2025-06-30 23:15:29 +0530

    randi rone

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://akshatastro.netlify.app",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [preact()]
 });
